77dff1f69c2d350b9fd8003e3b6c48f84942c239
Ashly Margaret <ashlymargaret030@gmail.com>
2022-11-02 13:03:31 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import PatternDivider from './images/pattern-divider-desktop.svg'
+import DiceImage from './images/icon-dice.svg'
+import axios from "axios";
+import { useState,useEffect } from 'react';
+import LoadingGIF from './images/loading.gif'
 
 function App() {
+
+ const [advice,setAdvice] = useState({})
+//  console.log("advice",advice);
+
+ const [loading,setLoading] = useState(true)
+
+ const [disableSubmitBtn, setDisableSubmitBtn] = useState(false);
+
+
+const GenerateAdvice =  ()=>{
+  setLoading(true)
+ 
+  // let id = Math.floor(Math.random() * 225);
+
+  setTimeout(()=>{
+    setDisableSubmitBtn(true)
+    axios.get(`https://api.adviceslip.com/advice`).then((response=>{
+    console.log(response.data.slip);
+    setAdvice(response.data.slip)
+    setLoading(false)
+   
+  })
+  )  // axios end
+  },1500)
+  setDisableSubmitBtn(false)
+}  // generateAdvice end
+
+
+useEffect(() => {
+    GenerateAdvice(); 
+},[]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="container">
+        {
+           loading ? <div><img src={LoadingGIF} alt="Loading" /></div> : <div>
+                                        <div className="idTitle">          
+                                        <h5 >ADVICE # {advice.id}</h5>          
+                                        </div>
+                                        <div className="advice">
+                                        <h3>{advice.advice}</h3>
+                                        </div>
+                                        </div>
+        }
+        
+        <div className="patternDivider">
+          <img src={PatternDivider} alt="" />
+        </div>
+        {
+              disableSubmitBtn ?   <div className="diceImage" onClick={GenerateAdvice}>
+                                     <img src={DiceImage} alt="" />
+                                    </div>   : null
+        }
+        
+           
+      </div>
+ 
     </div>
   );
 }
